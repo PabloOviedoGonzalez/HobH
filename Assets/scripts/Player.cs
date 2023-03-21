43506c218eafbd2370524baf3d50b5b17c81c67e
PabloOviedoGonzalez <pablooviedogonzalez@gmail.com>
2023-03-21 19:10:23 +0100

    start

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public KeyCode LeftArrow, RightArrow, UpArrow, DownArrow, Space; //cambiar las teclas desde el editor
+    public float speed = 10;
+    public SpriteRenderer spriteRenderer;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // accion teclas wasd ( arriba abajo izq drch + sollucion framerate
+
+        if (Input.GetKey(LeftArrow))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(RightArrow))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(UpArrow))
+        {
+
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(DownArrow))
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+    }
+}
